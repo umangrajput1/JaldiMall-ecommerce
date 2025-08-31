220328d25a9a7ec8d5875036157f815e444b5aac
Umang Rajput <umangkumar7800@gmail.com>
2025-08-31 23:54:17 +0530

    transactions Services and APIs build

---
+++ JaldiMall ( Backend )/JaldiMall/src/main/java/com/jaldimall/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.jaldimall.repository;
+
+import com.jaldimall.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findBySellerId(Long sellerId);
+}
