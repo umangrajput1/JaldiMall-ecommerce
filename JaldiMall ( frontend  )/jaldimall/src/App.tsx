abf0b67c0f03616644362e71b4149bbf70519b40
Umang Rajput <umangkumar7800@gmail.com>
2025-09-05 23:15:40 +0530

    start frontend implimentation

---
+++ JaldiMall ( frontend  )/jaldimall/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello jaldiMall website</h1>
+    </>
+  );
+}
+
+export default App;
