4c91c5b6bce13a5b43d3b668afd43c48ced987fb
Umang Rajput <umangkumar7800@gmail.com>
2025-09-07 22:34:11 +0530

    ready to implimention fronted

--- JaldiMall ( frontend  )/jaldimall/src/App.tsx
+++ JaldiMall ( frontend  )/jaldimall/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from "@mui/material";
 function App() {
   return (
     <>
       <h1 className='text-3xl font-bold underline'>Hello jaldiMall website</h1>
+      <Button variant="contained">Contained</Button>
     </>
   );
 }
